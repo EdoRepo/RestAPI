fd56ef25991a1c842084272bad0394cfc38e7118
Edo <negocio.erm@gmail.com>
2020-01-10 19:17:11 -0600

    Swagger

--- RestAPI/src/main/java/com/example/restapi/controller/ProductsController.java
+++ RestAPI/src/main/java/com/example/restapi/controller/ProductsController.java
@@ -3,6 +3,7 @@ package com.example.restapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.restapi.model.Product;
 import com.example.restapi.service.ProductsService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
+@Api(value = "ProductsControllerAPI", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProductsController {
 	
 	private ProductsService productsService;
@@ -30,6 +37,8 @@ public class ProductsController {
 	}
 	
 	@GetMapping(path = "/api/products/{id}")
+	@ApiOperation("Gest the product with specific id")
+	@ApiResponses( value = {@ApiResponse(code = 200, message = "OK", response = Product.class)})
 	public Product getProduct(@PathVariable(name = "id") String id) {
 		return productsService.getProduct(id);		
 	}
